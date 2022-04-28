e555bc243cbb274e132a64194013ef80079a59a7
Lana_K <lana1990.komar@gmail.com>
2022-04-28 11:21:17 -0700

    setTokenCookie, restoreUser and requireAuth implemented

--- EventWild/backend/routes/api/index.js
+++ EventWild/backend/routes/api/index.js
@@ -1,4 +1,37 @@
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+
+
+router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
+    const user = await User.findOne({
+        where: {
+            username: 'Demo'
+        }
+    });
+    setTokenCookie(res, user);
+    return res.json({ user });
+}));
+
+// GET /api/restore-user
+router.get(
+    '/restore-user',
+    restoreUser,
+    (req, res) => {
+        return res.json(req.user);
+    }
+);
+
+// GET /api/require-auth
+router.get(
+    '/require-auth',
+    requireAuth,
+    (req, res) => {
+        return res.json(req.user);
+    }
+);
 
 router.post('/test', function (req, res) {
     res.json({ requestBody: req.body });
