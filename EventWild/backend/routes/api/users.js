77d746754700eb6fff98ec4c8d5b6f429464b057
Lana_K <lana1990.komar@gmail.com>
2022-04-28 13:53:31 -0700

    signup validation implemented

--- EventWild/backend/routes/api/users.js
+++ EventWild/backend/routes/api/users.js
@@ -1,12 +1,39 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
+const { check } = require('express-validator');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { User } = require('../../db/models')
+const { User } = require('../../db/models');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
+const validateSignup = [
+    check('email')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide an email address.')
+        .isEmail()
+        .withMessage('Please provide a valid email.'),
+    check('username')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a username.')
+        .isLength({ min: 4 })
+        .withMessage('Please provide a username with at least 4 characters.'),
+    check('username')
+        .not()
+        .isEmail()
+        .withMessage('Username cannot be an email.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a password.')
+        .isLength({ min: 6 })
+        .withMessage('Password must be 6 characters or more.'),
+    handleValidationErrors
+];
+
+// Sign up
 router.post('/',
+    validateSignup,
     asyncHandler(async (req, res) => {
         const { email, password, username } = req.body;
 
